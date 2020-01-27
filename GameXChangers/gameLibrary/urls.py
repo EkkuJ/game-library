6d945ebd306a41fe0a938f62bc276bda565b6562
Mäkkäri Pro <arttu.maatta@aalto.fi>
2020-01-27 20:25:32 +0200

    add error and success urls

--- GameXChangers/gameLibrary/urls.py
+++ GameXChangers/gameLibrary/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('playGame/<int:game_id>', views.playGame, name='playGame'),
     path('buyGame/<int:game_id>', views.buyGame, name='buyGame'),
     path('addGame/', views.addGame, name='addGame'),
+    path('success/', views.success, name='success'),
+    path('error/', views.success, name='error'),
 ]
