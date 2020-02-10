8c7c09ae485b6de1d749aff106b3e45896b0fe0e
Miikka Tervo <miikka.tervo@gmail.com>
2020-02-10 21:05:29 +0200

    add django-heroku

--- GameXChangers/GameXChangers/settings.py
+++ GameXChangers/GameXChangers/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,3 +132,5 @@ LOGIN_REDIRECT_URL = '/gameLibrary/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
