cf414c8fde19a7a24e0cd6c80c728a95e4e0bf70
Miikka Tervo <miikka.tervo@gmail.com>
2020-02-11 21:21:04 +0200

    imporve playGame-view

--- GameXChangers/gameLibrary/views.py
+++ GameXChangers/gameLibrary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Game, OwnedGame
 from django.contrib.auth.models import User
 from .forms import GameForm
