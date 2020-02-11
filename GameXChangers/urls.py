6805c30921be0c393403f280487e2b81817bc69d
Miikka Tervo <miikka.tervo@gmail.com>
2020-02-11 14:04:07 +0200

    changed routing: deleted gameLibrary from urls

--- GameXChangers/urls.py
+++ GameXChangers/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gameLibrary/', include('gameLibrary.urls')),
+    path('', include('gameLibrary.urls')),
     path('authentication/', include('authentication.urls'))
 ]
