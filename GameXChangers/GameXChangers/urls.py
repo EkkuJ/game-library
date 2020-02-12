337038fd180d7b7800c3850e8767f393a3aa7fa1
Miikka Tervo <miikka.tervo@gmail.com>
2020-02-12 13:54:42 +0200

    add icon

--- GameXChangers/GameXChangers/urls.py
+++ GameXChangers/GameXChangers/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponseRedirect
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
