8971aae83af41b72dbe224da82e57d5693d4c607
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-02-15 18:12:56 +0200

    Add URL for group choice

--- GameXChangers/authentication/urls.py
+++ GameXChangers/authentication/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import activate
 
 from . import views
 
@@ -9,6 +8,7 @@ login_view = auth_views.LoginView.as_view()
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', views.register, name='register'),
-    path('activate/<uidb64>/<token>/', activate, name='activate'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('group/', views.group_choice, name='group_choice'),
     path('logout/', views.logout, name='logout')
 ]
