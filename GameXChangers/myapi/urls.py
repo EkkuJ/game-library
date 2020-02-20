1b6ef332e6364363bb6aa1f318af4934b3b08e4c
Mäkkäri Pro <arttu.maatta@aalto.fi>
2020-02-21 00:27:56 +0200

    take unnecessarystuff away

--- GameXChangers/myapi/urls.py
+++ GameXChangers/myapi/urls.py
@@ -3,16 +3,10 @@ from rest_framework import routers
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt import views as jwt_views
-#router = routers.DefaultRouter()
-#router.register(r'owneds', views.OwnedViewSet)
-
-
-# router.register(r'hello', views.HelloView)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    #path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('gameapi/', views.GameApiView.as_view(), name='gameapi'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
